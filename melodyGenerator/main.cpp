471733551577e32502556460a51a344347e024b1
Brayden William Albertini <bwalbertini@gmail.com>
2020-12-07 18:19:29 -0500

    Initial Commit

---
+++ melodyGenerator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  melodyGenerator
+//
+//  Created by Brayden Albertini on 12/7/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
